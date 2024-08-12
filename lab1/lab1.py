f5d9cd3db216f6c462f01ed3c5ffcb2aca553b61
Dmitriy Rechuk <68788658+Fanatsio@users.noreply.github.com>
2024-08-12 14:51:30 +0500

    Update lab1.py

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,40 +1,51 @@
+import numpy as np
+
 class Perceptron:
-    def __init__(self):
-        self.weights = [0.3, 0.2]
-        self.trainingSet = [[1, 1, 1], [1, 0, 1], [0, 1, 1], [0, 0, 0]]
-        self.accuracy = 0.1
+    def __init__(self, learning_rate=0.1):
+        self.training_inputs = np.array([
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1]
+        ])
+        self.labels = np.array([0, 0, 1, 0])
+        self.weights = np.random.rand(self.training_inputs.shape[1] + 1)  # Добавляем смещение
+        self.learning_rate = learning_rate
 
-    def train(self):
-        for i in range(len(self.trainingSet)):
-            d = self.trainingSet[i][2] - self.predict([self.trainingSet[i][0], self.trainingSet[i][1]])
-            if d != 0:
-                self.weights[0] += d * self.trainingSet[i][0] * self.accuracy
-                self.weights[1] += d * self.trainingSet[i][1] * self.accuracy
+        print(f"Начальные веса = {self.weights[1:]}, Смещение = {self.weights[0]}")
 
-    def test(self):
-        if (self.predict([0, 1]) == 1 and
-                self.predict([1, 0]) == 1 and
-                self.predict([1, 1]) == 1 and
-                self.predict([0, 0]) == 0):
-            return 1
-        return 0
+    def predict(self, inputs):
+        # Учитываем смещение при вычислении суммы
+        summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
+        return 1 if summation >= 0 else 0
 
-    def predict(self, x):
-        summer = x[0] * self.weights[0] + x[1] * self.weights[1]
-        if summer >= 1:
-            return 1
-        else:
-            return 0
+    def train_step(self):
+        total_error = 0
+        for inputs, label in zip(self.training_inputs, self.labels):
+            prediction = self.predict(inputs)
+            error = label - prediction
+            total_error += abs(error)
+            # Обновление весов и смещения
+            self.weights[1:] += self.learning_rate * error * inputs
+            self.weights[0] += self.learning_rate * error
+            print(f"Веса во время обучения {self.weights[1:]}, Смещение = {self.weights[0]}")
+        return total_error
 
-    def get_weight(self):
-        return self.weights
+    def test(self) -> bool:
+        correct_predictions = all(self.predict(inputs) == label for inputs, label in zip(self.training_inputs, self.labels))
+        return correct_predictions
 
+perceptron = Perceptron()
 
-neuron = Perceptron()
-print(f"Начальные веса = {neuron.get_weight()}\n")
+epoch = 0
+while not perceptron.test():
+    total_error = perceptron.train_step()
+    epoch += 1
+    print(f"Эпоха {epoch}, Общая ошибка: {total_error}")
 
-while neuron.test() != 1:
-    neuron.train()
-    print(f"Веса во время обучения {neuron.get_weight()}")
+print(f"Веса после обучения {perceptron.weights[1:]}, Смещение = {perceptron.weights[0]}")
 
-print(f"\nВеса после обучения {neuron.get_weight()}")
+print("Тестирование модели:")
+for inputs in perceptron.training_inputs:
+    prediction = perceptron.predict(inputs)
+    print(f"Вход: {inputs}, Предсказание: {prediction}")
